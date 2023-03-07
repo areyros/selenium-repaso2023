8bba9505bd0e9c674428c264fc5778e9b67917b3
rera_ <rera_@LAPTOP-TLS3V1C2>
2023-03-06 19:08:39 -0600

    clase de tipos de datos

---
+++ src/main/java/com/java/DataTypesRepaso.java
@@ -0,0 +1,31 @@
+package com.java;
+
+public class DataTypesRepaso {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		byte var1 = 127;
+		int var2 = 1;
+		short var3 = 2;
+		long var4 = 1000;
+		float var5 = 1.1f;
+		double var6 = 111.2222;
+		char var7 = 's';
+		boolean var8 = false;
+		String var9 = "cadena de caracteres";
+
+		System.out.println(var1 + " Este es el valor de la variable 1 byte");
+		System.out.println(var2 + " Este es el valor de la variable 2 int");
+		System.out.println(var3 + " Este es el valor de la variable 3 short");
+		System.out.println(var4 + " Este es el valor de la variable 4 long");
+		System.out.println(var5 + " Este es el valor de la variable 5 float");
+		System.out.println(var6 + " Este es el valor de la variable 6 double");
+		System.out.println(var7 + " Este es el valor de la variable 7 char");
+		System.out.println(var8 + " Este es el valor de la variable 8 boolean");
+		System.out.println(var9 + " Este es el valor de la variable 8 String");
+		
+		//concatenacion
+		System.out.println("Var 1 "+var1+" con var 2 "+var2+ " "+var9);
+	}
+}
